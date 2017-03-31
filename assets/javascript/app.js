7fc5fd8fb346626fa872a5d032addc4cf4cca8ca
Jimel J. Joseph <jaydev@Jimels-MacBook-Pro-2.local>
2017-03-31 07:25:09 -0400

    starting development with themoviedb api....

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,28 @@
-var api = 
+var apiKey = '45b404746d6aecd3a90cbeeeab19a52b';
+var imagePath = 'https://image.tmdb.org/t/p/original/';
+
+var queryUrlBase = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + apiKey +'&region=us';
+console.log('QUERYURLBASE', queryUrlBase);
+
+function runQuery(genre,queryURL){
+
+  $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(movieDBData){
+          for (var i = 0; i < movieDBData.results.length; i++) {
+
+            console.log(queryURL);
+            console.log(movieDBData.results[i]);
+            console.log('MOVIEDBDATA.RESULTS[I].TITLE', movieDBData.results[i].title);
+            console.log('MOVIEDBDATA.RESULTS[I].POSTER_IMAGES', imagePath +  movieDBData.results[i].poster_path);
+            
+          }
+
+
+
+        });
+
+}
+
+runQuery('test',queryUrlBase);
