08fa8328f9dabff673fd02a6f06f8fe77bfedb1b
atulsingla82 <asingla@wgu.edu>
2017-04-04 18:51:32 -0400

    Add files via upload

--- Trailer Zone/js/app.js
+++ Trailer Zone/js/app.js
@@ -1,84 +1,95 @@
 $(function() {
 
 
-            var Page = (function() {
-                var $navArrows = $('#nav-arrows').hide(),
-                    $shadow = $('#shadow').hide(),
-                    slicebox = $('#sb-slider').slicebox({
-  
+    
+
+
+
+
+    var Page = (function() {
+        var $navArrows = $('#nav-arrows').hide(),
+            $shadow = $('#shadow').hide(),
+            slicebox = $('#sb-slider').slicebox({
+
+    
+
+            onReady: function() {
+                    $navArrows.hide();
+                    $shadow.hide();
+
+
+
+                    $("#slider").hide();
+                    $("#videoModal").hide();
+                    $(".inputMovie").hide();
+                    $(".button").on('click', function() {
+
+                        $("#slider").show();
+                        $("#logIn").hide();
+                        $(".button").hide();
+                        $(".inputMovie").show();
+                        $("#shadow").show();
+                        $navArrows.show();
+
+
+                    });
+
+                    $(document).foundation();
+
+                    $("img").on("click", function() {
+
+                        $("#videoModal").foundation('open').fadeIn();
 
 
 
 
 
 
-                        onReady: function() {
-                            $navArrows.hide();
-                            $shadow.hide();
+                        $("#slider").fadeOut();
+                        $("#logIn").fadeOut();
+                        $(".button").fadeOut();
+                        $("#shadow").fadeOut();
+                        $navArrows.fadeOut();
+                        $(".header").fadeOut();
+                        $(".inputMovie").hide();
+
+                        return false;
+                    });
+
+                    $('a.close').on('click', function() {
+                        $('#videoModal').foundation('close');
+                        $(".header").show();
+                        $("#slider").show();
+                        $("#shadow").show();
+                        $navArrows.show();
+                        $(".inputMovie").show();
+
+                    });
 
 
+                },
+                orientation: 'r',
+                cuboidsRandom: true,
+                disperseFactor: 30
+            }),
+            init = function() {
+                initEvents();
+            },
+            initEvents = function() {
 
-                            $("#slider").hide();
-                            $("#videoModal").hide();
-
-                            $(".button").on('click', function() {
-
-                                $("#slider").show();
-                                $("#logIn").hide();
-                                $(".button").hide();
-                                $("#shadow").show();
-                                $navArrows.show();
-
-
-                            });
-
-                            $(document).foundation();
-
-                            $("img").on("click", function() {
-
-                                $("#videoModal").foundation('open');
-                                
-
-
-
-                                $("#slider").fadeOut(1000);
-                                $("#logIn").fadeOut();
-                                $(".button").fadeOut();
-                                $("#shadow").fadeOut();
-                                $navArrows.fadeOut();
-                                $(".header").fadeOut();
-
-                                return false;
-
-                                
-
-                                
-                            });
-
-
-                        },
-                        orientation: 'r',
-                        cuboidsRandom: true,
-                        disperseFactor: 30
-                    }),
-                    init = function() {
-                        initEvents();
-                    },
-                    initEvents = function() {
-
-                        // add navigation events
-                        $navArrows.children(':first').on('click', function() {
-                            slicebox.next();
-                            return false;
-                        });
-                        $navArrows.children(':last').on('click', function() {
-                            slicebox.previous();
-                            return false;
-                        });
-                    };
-                return {
-                    init: init
-                };
-            })();
-            Page.init();
-        });
+                // add navigation events
+                $navArrows.children(':first').on('click', function() {
+                    slicebox.next();
+                    return false;
+                });
+                $navArrows.children(':last').on('click', function() {
+                    slicebox.previous();
+                    return false;
+                });
+            };
+        return {
+            init: init
+        };
+    })();
+    Page.init();
+});
--- Trailer Zone/js/jquery.slicebox.js
+++ Trailer Zone/js/jquery.slicebox.js
@@ -157,17 +157,17 @@ $.Slicebox = function(options, element) {
 
 $.Slicebox.defaults = {
     // (v)ertical, (h)orizontal or (r)andom
-    orientation: 'v',
+    orientation: 'h',
     // perspective value
     perspective: 1200,
     // number of slices / cuboids
     // needs to be an odd number 15 => number > 0 (if you want the limit higher, change the _validate function).
-    cuboidsCount: 5,
+    cuboidsCount: 10,
     // if true then the number of slices / cuboids is going to be random (cuboidsCount is overwitten)
     cuboidsRandom: false,
     // the range of possible number of cuboids if cuboidsRandom is true
     // it is strongly recommended that you do not set a very large number :)
-    maxCuboidsCount: 5,
+    maxCuboidsCount: 10,
     // each cuboid will move x pixels left / top (depending on orientation). The middle cuboid doesn't move. the middle cuboid's neighbors will move disperseFactor pixels
     disperseFactor: 0,
     // color of the hidden sides
@@ -177,7 +177,7 @@ $.Slicebox.defaults = {
     sequentialFactor: 150,
     // animation speed
     // this is the speed that takes "1" cuboid to rotate
-    speed: 600,
+    speed: 700,
     // transition easing
     easing: 'ease',
     // if true the slicebox will start the animation automatically
@@ -296,7 +296,7 @@ $.Slicebox.prototype = {
 
         if (this.options.orientation !== 'v' && this.options.orientation !== 'h' && this.options.orientation !== 'r') {
 
-            this.options.orientation = 'v';
+            this.options.orientation = 'h';
 
         }
 
